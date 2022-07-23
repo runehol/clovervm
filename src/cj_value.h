aa3312fb106250073bb3878da0e505d8d190dab2
Rune Holm <rune.holm@gmail.com>
2022-07-23 21:07:03 +0200

    initial version

---
+++ src/cj_value.h
@@ -0,0 +1,39 @@
+#ifndef CJ_VALUE_H
+#define CJ_VALUE_H
+
+#include <stdint.h>
+#include <assert.h>
+#include <stdbool.h>
+
+struct cj_object;
+
+typedef union cj_value
+{
+    int64_t smi_value;
+    struct cj_object *ptr_value;
+
+} cj_value;
+
+static inline bool value_is_smi(cj_value val)
+{
+    return (val.smi_value &0x1) == 0;
+}
+static inline bool value_is_oop(cj_value val)
+{
+    return (val.smi_value &0x1) == 1;
+}
+
+static inline int64_t value_get_smi(cj_value val)
+{
+    assert(value_is_smi(val));
+    return val.smi_value >> 1;
+}
+
+static inline struct CJObject *value_get_oop(cj_value val)
+{
+    assert(value_is_oop(val));
+    return (struct CJObject *)(val.smi_value - 1);
+}
+
+
+#endif //CJ_VALUE_H
---
+++ src/clojit.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "cj_object.h"
+#include "cj_value.h"
+
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}
