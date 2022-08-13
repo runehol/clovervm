87613f67cc311ac5effd8ad971779c393a2b3964
Rune Holm <rune.holm@gmail.com>
2022-08-13 23:42:54 +0200

    correct true/false literals

--- src/cl_value.h
+++ src/cl_value.h
@@ -89,9 +89,9 @@ static inline bool value_is_falsy(cl_value val)
 }
 
 static const cl_value cl_nil = (cl_value){.v=0x43};
-static const cl_value cl_true = (cl_value){.v=0x21};
+static const cl_value cl_true = (cl_value){.v=0x22};
 static const cl_value cl_false = (cl_value){.v=0x23};
-static const cl_value cl_exception_marker = (cl_value){.v=0x81}; // special value to return if an exception has been thrown.
+static const cl_value cl_exception_marker = (cl_value){.v=0x82}; // special value to return if an exception has been thrown.
 
 
 #endif //CL_VALUE_H
