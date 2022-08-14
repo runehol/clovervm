4541b78b00e47eaea39099a3715dabdb8f3e6057
Rune Holm <rune.holm@gmail.com>
2022-08-14 13:24:04 +0200

    started tokenizer

---
+++ src/tokenizer.h
@@ -0,0 +1,22 @@
+#ifndef CL_TOKENIZER_H
+#define CL_TOKENIZER_H
+
+typedef enum token_kind
+{
+    TK_LPAREN,
+    TK_RPAREN,
+    TK_LBRACKET,
+    TK_RBRACKET,
+    TK_INTEGER,
+    TK_STRING,
+    TK_SYMBOL,
+    TK_EOF
+}
+
+typedef struct token
+{
+
+} token;
+
+
+#endif //CL_TOKENIZER_H
