45099b1bf9716a924d6d07e30f560c875384b8eb
Rune Holm <rune.holm@gmail.com>
2023-02-23 02:12:34 +0100

    adjusted truthyness test to match python semantics

--- src/cl_value.h
+++ src/cl_value.h
@@ -18,13 +18,13 @@ struct CLObject;
     Bit 3 (8): Permanent (non-refcounted) pointer
     Bit 2 (4): Interned pointer
     Bit 1 (2): Special value
-    Bit 0 (1): Falsy value
+    Bit 0 (1): Special truthy value
 
     The tags then are as follows:
     00000: small integer, with the value is stored as signed two complement in the upper 59 bits.
-    00010: special truthy value. Only one known: 0x22: True
-    00011: special falsy value. Two known: 0x23: False, 0x43: None
-    00100: interned pointer
+    00011: special truthy value. Only one known: 0x23: True
+    00010: special falsy value. Two known: 0x22: False, 0x42: None
+    00100: interned pointer.
     01000: permanent pointer
     10000: refcounted pointer
 
@@ -38,7 +38,7 @@ static const uint64_t cl_refcounted_ptr_tag = 0x10;
 static const uint64_t cl_permanent_ptr_tag  = 0x08;
 static const uint64_t cl_interned_ptr_tag   = 0x04;
 static const uint64_t cl_special_tag        = 0x02;
-static const uint64_t cl_falsy_tag          = 0x01;
+static const uint64_t cl_truthy_tag          = 0xffffffffffffffe1ull;
 static const uint64_t cl_ptr_mask = cl_refcounted_ptr_tag|cl_permanent_ptr_tag|cl_interned_ptr_tag;
 
 typedef union CLValue
@@ -98,17 +98,18 @@ static inline struct CLObject *value_get_ptr(CLValue val)
 
 static inline bool value_is_truthy(CLValue val)
 {
-    return (val.v&cl_falsy_tag) == 0;
+    return (val.v&cl_truthy_tag) != 0;
 }
 static inline bool value_is_falsy(CLValue val)
 {
-    return (val.v&cl_falsy_tag) != 0;
+    return (val.v&cl_truthy_tag) == 0;
 }
 
-static const CLValue cl_None = (CLValue){.v=0x43};
-static const CLValue cl_True = (CLValue){.v=0x22};
-static const CLValue cl_False = (CLValue){.v=0x23};
-static const CLValue cl_exception_marker = (CLValue){.v=0x82}; // special value to return if an exception has been thrown.
+static constexpr CLValue cl_None = (CLValue){.v=0x42};
+static constexpr CLValue cl_False = (CLValue){.v=0x22};
+static constexpr CLValue cl_True = (CLValue){.v=0x23};
+static constexpr CLValue cl_exception_marker = (CLValue){.v=0x82}; // special value to return if an exception has been thrown.
+static constexpr CLValue cl_tombstone = (CLValue){.v=0x102}; // dead value
 
 
 #endif //CL_VALUE_H
