e0c552563592047e35e685d5dddcfcf65854d60d
Rune Holm <rune.holm@gmail.com>
2023-03-11 10:55:54 +0100

    added command line runner

---
+++ benchmark/microbench2.py
@@ -0,0 +1,8 @@
+counter = 100000001
+acc = 1245
+while counter:
+    acc = (-acc*64 + 64)>>6
+
+    counter -= 1
+
+acc
--- src/clovervm.cpp
+++ src/clovervm.cpp
@@ -5,9 +5,63 @@
 #include "short_vector.h"
 #include "string.h"
 #include "refcount.h"
+#include <sstream>
+#include <fstream>
+#include <codecvt>
+#include "virtual_machine.h"
+#include "thread_state.h"
+#include "parser.h"
+#include "code_object_print.h"
+#include <fmt/xchar.h>
 
-int main()
+using namespace cl;
+
+
+std::wstring read_file(const char* filename)
 {
-    fmt::print("hello world\n");
+    std::wifstream wif(filename);
+    wif.imbue(std::locale(std::locale(), new std::codecvt_utf8<wchar_t>));
+    std::wstringstream wss;
+    wss << wif.rdbuf();
+    return wss.str();
+}
+
+
+int main(int argc, const char *argv[])
+{
+    using namespace std::literals;
+    bool print_bytecode = false;
+    const char *source_file = nullptr;
+    int i = 1;
+    for(; i < argc; ++i)
+    {
+        if(argv[i] == "--print-bytecode"sv)
+        {
+            print_bytecode = true;
+        } else {
+            break;
+        }
+    }
+
+    if(i < argc)
+    {
+        source_file = argv[i++];
+    }
+
+    // read the file and execute it
+
+    if(source_file != nullptr)
+    {
+        VirtualMachine vm;
+        std::wstring file_contents = read_file(source_file);
+        CodeObject code_obj = vm.get_default_thread()->compile(file_contents.c_str(), StartRule::File);
+        if(print_bytecode)
+        {
+            std::cout << fmt::to_string(code_obj) << "\n";
+        }
+        Value v = vm.get_default_thread()->run(&code_obj);
+        std::cout << v.get_smi() << "\n";
+    }
+
     return 0;
 }
