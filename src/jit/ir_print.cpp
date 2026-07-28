fd9de72039ab33cbfe45fdabb8e547a86582d4a7
Rune Holm <rune.holm@gmail.com>
2026-07-28 16:57:28 +0100

    Print side exit bodies in JIT IR

--- src/jit/ir_print.cpp
+++ src/jit/ir_print.cpp
@@ -41,6 +41,13 @@ namespace cl::jit
                         add_result(instruction);
                     }
                 }
+                for(const SideExit *side_exit: graph.side_exits())
+                {
+                    for(InstructionId instruction: side_exit->instructions())
+                    {
+                        add_result(graph.storage()->instruction(instruction));
+                    }
+                }
             }
 
             size_t block_id(const Block *block) const
@@ -450,6 +457,36 @@ namespace cl::jit
             {
                 fmt::format_to(std::back_inserter(out), "  ");
                 print_instruction(out, state, instruction);
+                fmt::format_to(std::back_inserter(out), "\n");
+            }
+        }
+        for(size_t side_exit_index = 0;
+            side_exit_index < graph.side_exits().size(); ++side_exit_index)
+        {
+            const SideExit &side_exit = graph.side_exit(
+                SideExitId(static_cast<uint32_t>(side_exit_index)));
+            fmt::format_to(std::back_inserter(out),
+                           "\n"
+                           "s{} inputs(",
+                           side_exit_index);
+            for(size_t input_index = 0; input_index < side_exit.inputs().size();
+                ++input_index)
+            {
+                if(input_index != 0)
+                {
+                    fmt::format_to(std::back_inserter(out), ", ");
+                }
+                print_parameter(
+                    out, state,
+                    graph.storage()->instruction(
+                        side_exit.inputs()[input_index].instruction_id()));
+            }
+            fmt::format_to(std::back_inserter(out), "):\n");
+            for(InstructionId instruction_id: side_exit.instructions())
+            {
+                fmt::format_to(std::back_inserter(out), "  ");
+                print_instruction(out, state,
+                                  graph.storage()->instruction(instruction_id));
                 fmt::format_to(std::back_inserter(out), "\n");
             }
         }
